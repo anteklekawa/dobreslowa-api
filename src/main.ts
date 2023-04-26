fa69cc10912166bc8087274ebe1c5a04b4370c07
antostibijka <anteklekawa3@gmail.com>
2023-04-26 20:34:11 +0200

    xd

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('dobreslowa-api')
+    .setDescription('#dobreslowa - swagger')
+    .setVersion('1.0')
+    .addTag('')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   app.setGlobalPrefix('api');
   app.use(cookieParser());
   app.enableCors({
