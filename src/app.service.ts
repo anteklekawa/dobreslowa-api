81830e30867cd1b2452b583071f3fcc4bc93bde2
antostibijka <anteklekawa3@gmail.com>
2023-04-26 18:32:40 +0200

    xd

--- src/app.service.ts
+++ src/app.service.ts
@@ -97,7 +97,7 @@ export class AppService {
           }
         })
         throw new ForbiddenException('Access token is expired!');
-      }, 10000);
+      }, 600000);
 
       delete user.password;
 
@@ -140,7 +140,7 @@ export class AppService {
           }
         })
         throw new ForbiddenException('Access token is expired!');
-      }, 10000);
+      }, 600000);
 
       return { accessToken: newAccessToken }
     }
